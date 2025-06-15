332c511450425a1a78eb097d65e2cae9a95a207e
DhruvVaghani <108922829+DhruvVaghani@users.noreply.github.com>
2025-06-14 21:39:19 -0400

    Update personal_agent.py

--- personal_agent.py
+++ personal_agent.py
@@ -20,8 +20,8 @@ from dotenv import load_dotenv
 load_dotenv()
 from supabase import create_client
 import os
-supabase_url = st.secrets["SUPABASE_URL"]
-supabase_key = st.secrets["SUPABASE_KEY"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 github_token = st.secrets["GITHUB_ACCESS_TOKEN"]
 # SUPABASE_URL = os.getenv("SUPABASE_URL")
 # SUPABASE_KEY = os.getenv("SUPABASE_KEY")
